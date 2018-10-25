8932b6b9607891f1abdbb05a88dc9ace7e58a2a0
Rishab Pohane <pohanerishab@gmail.com>
2018-10-25 18:09:57 -0500

    App finally builds properly

--- Theta Tau/Theta Tau.Android/MainActivity.cs
+++ Theta Tau/Theta Tau.Android/MainActivity.cs
@@ -10,18 +10,13 @@ using Android.OS;
 namespace Theta_Tau.Droid
 {
     [Activity(Label = "Theta_Tau", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
-
             base.OnCreate(savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new Xamarin.Forms.Application());
-            //Rishab likes dish soap
-            PrintService("Hello World");
+            Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
         }
     }
 }
